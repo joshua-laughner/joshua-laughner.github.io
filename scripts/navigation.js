9d526a3f6c70da2bc88d7c8bee5cca8f188e3dfb
Joshua Laughner <jllacct119@gmail.com>
2020-05-11 11:04:22 -0700

    Added contact page with GnuPG public key

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,6 +2,9 @@ var site_url = document.URL;
 // Get the site root assuming that the root ends in github.io; that substring plus
 // the trailing slash is 10 characters
 var site_root = site_url.substr(0, site_url.lastIndexOf('github.io')+10)
+var github = 'https://github.com/joshua-laughner'
+var orcid = 'https://orcid.org/0000-0002-8599-4555'
+var linkedin = 'http://www.linkedin.com/in/joshua-laughner'
 
 document.getElementById("navMenu").innerHTML=
 '<a class="navbar-brand" href="' + site_root + 'index.html' +'">JLL</a>'+
@@ -28,6 +31,9 @@ document.getElementById("navMenu").innerHTML=
     '<li class="nav-item active">'+
       '<a class="nav-link" href="' + site_root + 'files/Laughner-CV.pdf' + '">CV</a>'+
     '</li>'+
+    '<li class="nav-item active">'+
+      '<a class="nav-link" href="' + site_root + 'site/contact.html' + '">Contact</a>'+
+    '</li>'+
   '</ul>'+
 '</div>'
 
@@ -36,9 +42,9 @@ document.getElementById("myfooter").innerHTML =
 '<div class="row">'+
 '<div class="col-sm-1"></div>'+
 '<div class="col-sm-10" align="center">'+
-  '<a href="http://www.linkedin.com/in/joshua-laughner" target="_blank"><img src="' + site_root + 'img/logos/In-2C-66px-TM.png' + '"></a>'+
+  '<a href="' + linkedin + '" target="_blank"><img src="' + site_root + 'img/logos/In-2C-66px-TM.png' + '"></a>'+
   '&nbsp;&nbsp;&nbsp;'+
-  '<a href="https://github.com/joshua-laughner" target="_blank"><img style="max-height:66px" src="' + site_root + 'img/logos/GitHub_Logo.png' +'"></a>'+
+  '<a href="' + github + '" target="_blank"><img style="max-height:66px" src="' + site_root + 'img/logos/GitHub_Logo.png' +'"></a>'+
 '</div>'+
 '<div class="col-sm-1"></div>'+
 '</div>'+
@@ -46,8 +52,22 @@ document.getElementById("myfooter").innerHTML =
 '<div class="row">'+
 '<div class="col-sm-1"></div>'+
 '<div class="col-sm-10" align="center">'+
-  '<a href="https://orcid.org/0000-0002-8599-4555" target="_blank"><img src="' + site_root + 'img/logos/orcid_16x16.png' + '">  https://orcid.org/0000-0002-8599-4555</a>'+
+  '<a href="' + orcid + '" target="_blank"><img src="' + site_root + 'img/logos/orcid_16x16.png' + '">  ' + orcid + '</a>'+
 '</div>'+
 '<div class="col-sm-1"></div>'+
 '</div>'+
 '<div class="row">&nbsp;</div>'
+
+var linkedin_a = document.getElementById("linkedin");
+var github_a = document.getElementById("github");
+var orcid_a = document.getElementById("orcid");
+
+if (linkedin_a) {
+    linkedin_a.setAttribute("href", linkedin);
+}
+if (github_a) {
+    github_a.setAttribute("href", github);
+}
+if (orcid_a) {
+    orcid_a.setAttribute("href", orcid);
+}
